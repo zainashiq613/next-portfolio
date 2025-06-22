e7bb829cc758e33623743671baae68748964799c
Muhammad Zain <zain.ashiq613@gmail.com>
2025-06-22 22:47:34 +0500

    hero and grid

---
+++ components/Hero.jsx
@@ -0,0 +1,49 @@
+import { FaLocationArrow } from 'react-icons/fa6';
+import { cn } from '@/lib/utils';
+
+import MagicButton from './ui/MagicButton';
+import { Spotlight } from './ui/Spotlight';
+import { TextGenerateEffect } from './ui/TextGenerateEffect';
+
+const Hero = () => {
+  return (
+    <div className="pb-20">
+      <div>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="relative h-[50rem] flex w-full items-center justify-center bg-white dark:bg-[#000319]">
+        <div
+          className={cn(
+            'absolute inset-0',
+            '[background-size:40px_40px]',
+            '[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]',
+            'dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]'
+          )}
+        />
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-[#000319]"></div>
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+              Dynamic Web Magic with Next.js
+            </p>
+            <TextGenerateEffect
+              words="Transforming Concepts into Seamless User Experiences"
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            />
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi! I'm Zain Ashiq.
+            </p>
+            <a href="#about">
+              <MagicButton title="Show my work" icon={<FaLocationArrow />} position="right" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ components/ui/BentoGrid.jsx
@@ -0,0 +1,73 @@
+import { cn } from '@/lib/utils';
+import { BackgroundGradient } from './GradientBg';
+
+export const BentoGrid = ({ className, children }) => {
+  return (
+    <div
+      className={cn(
+        'mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-5',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}) => {
+  return (
+    <div
+      className={cn(
+        'row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4',
+        className
+      )}
+      style={{
+        background: 'rgb(4,7,29)',
+        backgroundColor: 'linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)',
+      }}
+    >
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <img src={img} alt={img} className={cn(imgClassName, 'object-cover object-center ')} />
+          )}
+        </div>
+        <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'} `}>
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full"
+            />
+          )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradient>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+          </BackgroundGradient>
+        )}
+
+        <div
+          className={cn(
+            titleClassName,
+            'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+          )}
+        >
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+            {description}
+          </div>
+          <div className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}>{title}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
